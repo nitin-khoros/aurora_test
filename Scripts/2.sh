d5e5cd4ab039419e1d396c75f26e9b9ea2ce315c
nitin-khoros <99383810+nitin-khoros@users.noreply.github.com>
2022-11-09 14:33:48 +0530

    Update 2.sh

--- Scripts/2.sh
+++ Scripts/2.sh
@@ -2,4 +2,4 @@
 
 echo "This is Script 2"
 echo "Username & password is $CREDS"
-curl --silent http://$CREDS@jenkins.dev.lithium.com/job/LIA_PACKAGE_JAVA8_BUILD/lastBuild/api/json -H 'ContentType: text/xml'
+# curl --silent http://$CREDS@jenkins.dev.lithium.com/job/LIA_PACKAGE_JAVA8_BUILD/lastBuild/api/json -H 'ContentType: text/xml'
