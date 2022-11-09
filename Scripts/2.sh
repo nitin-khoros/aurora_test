c948e18b89bd087e3fe671b3c0b4bd66afbd6bac
nitin-khoros <99383810+nitin-khoros@users.noreply.github.com>
2022-11-09 17:19:06 +0530

    Update 2.sh

--- Scripts/2.sh
+++ Scripts/2.sh
@@ -4,5 +4,6 @@ source env.properties
 echo "This is Script 2"
 echo "Username & password is $CREDS"
 echo "value of VAR1 fetched from script 1 is $VAR1"
+echo VAR2=two >> env.properties
 
 # curl --silent http://$CREDS@jenkins.dev.lithium.com/job/LIA_PACKAGE_JAVA8_BUILD/lastBuild/api/json -H 'ContentType: text/xml'
