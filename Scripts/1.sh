5ba4acad2785e17c07cc440cfbe430d96a66ef7d
nitin-khoros <99383810+nitin-khoros@users.noreply.github.com>
2022-11-09 14:52:14 +0530

    Update 1.sh

--- Scripts/1.sh
+++ Scripts/1.sh
@@ -2,8 +2,8 @@
 
 echo "This is Script 1"
 VAR1=one
-touch env.vars
-echo VAR1=one > env.vars
+touch envFile
+echo VAR1=one >> envFile
 echo "***from shell script"
 echo "VAR1 value is $VAR1"
 echo "***from shell script"
